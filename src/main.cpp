6d2db130e3942f6f3d0216d03628adb59902d2d4
Tobias Eriksson <162911+tobier@users.noreply.github.com>
2026-07-23 15:26:58 +0200

    Disable self test per default since I now see that the panels and lights work

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,37 @@
 #include "MainDisplay.h"
 #include "OptionDisplays.h"
 
+// Boot self test runs unless platformio.ini defines ENABLE_SELF_TEST=0.
+#ifndef ENABLE_SELF_TEST
+#define ENABLE_SELF_TEST 1
+#endif
+
+#define SELF_TEST_HOLD_MS 2500
+
+static void runSelfTest()
+{
+#if ENABLE_SELF_TEST
+  MainDisplay::selfTest();
+  OptionDisplays::selfTest();
+  Lights::selfTest();
+
+  delay(SELF_TEST_HOLD_MS);
+
+  MainDisplay::clear();
+  OptionDisplays::clearAll();
+  OptionDisplays::flushAll();
+#endif
+}
+
 void setup()
 {
   Serial.begin(COM_BAUD_RATE);
 
   MainDisplay::init();
-  MainDisplay::selfTest();
-
   OptionDisplays::init();
-  OptionDisplays::selfTest();
-
   Lights::init();
-  Lights::selfTest();
 
-  delay(2500);
-
-  MainDisplay::clear();
-  OptionDisplays::clearAll();
-  OptionDisplays::flushAll();
+  runSelfTest();
 }
 
 void loop()
